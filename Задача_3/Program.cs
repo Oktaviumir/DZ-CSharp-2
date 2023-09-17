a082f8a381ce50ec6a60decf6a0645560d804e64
Oktavium <oktaviumir@yandex.ru>
2023-09-17 10:47:03 +0300

    Решили задачу 3

--- Задача_3/Program.cs
+++ Задача_3/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] num = new double[size];
+FillArrayRandomNum(num);
+Console.WriteLine("массив: ");
+PrintArray(num);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+void FillArrayRandomNum(double[] num)
+{
+    for(int i = 0; i < num.Length; i++)
+        {
+            num[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+
+void PrintArray(double[] num)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < num.Length; i++)
+        {
+            Console.Write(num[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+for (int i = 0; i < num.Length; i++)
+{
+    if (num[i] > max)
+        {
+            max = num[i];
+        }
+    if (num[i] < min)
+        {
+            min = num[i];
+        }
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
